3348091ce22f3ecf9817b61835b463ba6ea6695d
Dexter Carpenter <dexter.carpenter03@gmail.com>
2026-06-23 17:53:50 +0000

    add tests

---
+++ Testing/test.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <iostream>
+
+#include "../src/board/board.h"
+
+int main() {
+    /* ========== COMMON TESTS ========== */
+    assert(SQ_NB == 64);
+    assert(FILE_OF[A1] == 'a');
+    assert(FILE_OF[D6] == 'd');
+    assert(RANK_OF[A1] == '1');
+    assert(RANK_OF[E3] == '3');
+    assert(isLightSquare(A1) == false);
+    assert(isLightSquare(B7) == true);
+    assert(PIECE_SYMBOL[WHITE][PAWN] == "♙");
+    assert(PIECE_SYMBOL[BLACK][QUEEN] == "♛");
+    assert(SQUARE_SYMBOL[WHITE] == "◻");
+    assert(SQUARE_SYMBOL[BLACK] == "◼");
+
+    /* ========== PIECE TESTS ========== */
+    Piece white_pawn(WHITE, PAWN);
+    assert(white_pawn.getColor() == WHITE);
+    assert(white_pawn.getPiece() == PAWN);
+    Piece black_king(BLACK, KING);
+    assert(black_king.getColor() == BLACK);
+    assert(black_king.getPiece() == KING);
+
+    /* ========== MOVE TESTS ========== */
+    Move move1(A2, A4);
+    assert(move1.from_square == A2);
+    assert(move1.to_square == A4);
+    assert(move1.promotion_piece == PIECE_NB);
+
+    /* ========== BOARD TESTS ========== */
+
+
+
+
+
+
+
+
+    return 0;
+}
